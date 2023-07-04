6e34063e3739d6796f179ce2e2108fdcbf4effe8
MathiasChirivi <chirivimathias3@gmail.com>
2023-07-04 18:44:52 +0200

    fix code and add layout

--- resources/views/Fumetti/index.blade.php
+++ resources/views/Fumetti/index.blade.php
@@ -5,11 +5,16 @@
     <h1>Welcome Page</h1>
     <div class="row g-4">
         <div class="col">
-            <div>
-                <ul>
+            <div >
+                <ul class="d-flex flex-wrap gap-4">
                     @foreach ($fumetti as $fumetto)
-                    <li>
-                        <a href="{{route("fumetti.show", $fumetto->id)}}">{{$fumetto->title}}</a>
+                    <li class="list-unstyled">
+                        <div class="card cardBg" style="height: 32rem; width: 18rem;">
+                            <img class="h-75" src="{{$fumetto->thumb}}" alt="">
+                        <div class="card-body">
+                            <a class="d-flex justify-content-center text-white text-decoration-none" href="{{route("fumetti.show", $fumetto->id)}}">{{$fumetto->title}}</a>
+                        </div>
+                        </div>
                     </li>                    
                     @endforeach
                 </ul>
